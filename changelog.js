6c8e3b5c2de38f9652c411f932469b3363baf243
Francois Beaufort <beaufort.francois@gmail.com>
2013-03-23 04:06:23 -0400

    Added a request permissions button into the changelog page

--- changelog.js
+++ changelog.js
@@ -1,5 +1,11 @@
 var changelog = document.getElementById('changelog');
 
+var sorry = document.getElementById('sorry');
+sorry.innerText = chrome.i18n.getMessage('requestPermissionsText');
+
+var showChangelogButton = document.getElementById('showChangelogButton');
+showChangelogButton.innerText = chrome.i18n.getMessage('changelogButtonTitle');
+
 var extension = null;
 var extensionId = window.location.hash.substr(1);
 
@@ -9,16 +15,16 @@ chrome.management.get(extensionId, function(extension) {
 });
 
 function showChangelog() {
-// Display loading message
-changelog.innerHTML = chrome.i18n.getMessage('loadingChangelogText');
+  showChangelogButton.style.display = 'none';
+  sorry.style.display = 'none';
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://chrome.google.com/webstore/detail/' + extensionId, true);
-xhr.setRequestHeader("Cache-Control","no-cache,max-age=0");
+  // Display loading message
+  changelog.innerText = chrome.i18n.getMessage('loadingChangelogText');
 
-xhr.onreadystatechange = function(e) {
-  if (this.readyState == 4 && this.status == 200) {
-    var r = this.response;
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://chrome.google.com/webstore/detail/' + extensionId, true);
+  xhr.onload = function() {
+    var r = xhr.response;
     // Check what's inside <pre></pre> tags
     var text = r.substring(r.search('<pre '), r.search('</pre>')+6);
 
@@ -28,28 +34,28 @@ xhr.onreadystatechange = function(e) {
       changelog.innerHTML = text.substring(text.search(/changelog/i), text.length-6);
     } else {
       changelog.style.display = 'none';
-      document.getElementById('sorry').innerHTML = chrome.i18n.getMessage('sorryChangelogText');
+      sorry.style.display = 'block';
+      sorry.innerText = chrome.i18n.getMessage('sorryChangelogText');
     }
-  }
-};
-
-xhr.send(null);
-
+  };
+  xhr.send(null);
 }
+
+
+// Permissions handling
 
 var permissions = { origins: ['https://chrome.google.com/*'] };
 
-chrome.permissions.contains(permissions, function(hasPermissions) {
-  if (hasPermissions) {
+showChangelogButton.addEventListener('click', function() {
+  chrome.permissions.request(permissions, function(hasGranted) {
+    if (hasGranted) {
+      showChangelog();
+    }
+  });
+});
+
+chrome.permissions.contains(permissions, function(result) {
+  if (result) {
     showChangelog();
-  } else {
-    changelog.innerHTML = 'This feature requests an additional permission. Click anywhere to get permissions prompt';
-    document.addEventListener('click', function() {
-      chrome.permissions.request(permissions, function(granted) {
-        if (granted) {
-          showChangelog();
-        }
-      });
-    });
   }
 });
