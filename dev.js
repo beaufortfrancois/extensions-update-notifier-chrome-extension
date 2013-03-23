a293bf8c9226f7bddea1f470443e144273d6b16a
Francois Beaufort <beaufort.francois@gmail.com>
2013-03-23 03:28:39 -0400

    Added new file for development environment

---
+++ dev.js
@@ -0,0 +1,4 @@
+localStorage.hfhhnacclhffhdffklopdkcgdhifgngh = '0';
+localStorage.knmdbhdejcjgpahocbnbbekpaehgghnk = '0';
+localStorage.bebigdkelppomhhjaaianniiifjbgocn = '0';
+localStorage.eignhdfgaldabilaaegmdfbajngjmoke = '0';
