61da449014e8491bfc30676f947544a6fcf1bfe9
Francois Beaufort <beaufort.francois@gmail.com>
2015-02-04 16:02:48 +0100

    Localized options page

--- src/options.js
+++ src/options.js
@@ -8,8 +8,19 @@ function saveOptions() {
   });
 }
 
+// Localize all content.
+function localize() {
+  var elements = document.querySelectorAll('[i18-content]');
+  for (var element, i = 0; element = elements[i]; i++) {
+    var messageName = element.getAttribute('i18-content');
+    element.textContent = chrome.i18n.getMessage(messageName);
+  }
+}
+
 // Restores synced preferences.
 window.onload = function() {
+  localize();
+
   var defaultOptions = {
     alwaysDisableExtension: false
   };
